0a9ca8e7c5ab02663a4cc52aeb2483009848c480
Gary <gkozlowskibusiness@outlook.com>
2023-10-22 20:26:39 -0400

    commit 2

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -10,9 +10,7 @@ const About = () => {
                     <p className="text-xs lg:text-md tracking-wider">The MetverseOfThings (MVoT) is a pioneering company at the forefront of blockchain research and deployment, firmly rooted in the practical applications of this groundbreaking technology in the real world. 
                     Our core mission is as ambitious as it is vital: to ensure that blockchain is seamlessly integrated into the fabric of society, thereby fostering benefits for all of humanity.
                     </p>
-                    <p className="text-xs lg:text-md pt-2 tracking-wider">In a world where trust, security, and transparency have never been more critical, blockchain technology represents a transformative force. 
-                    MVoT recognizes the immense potential it holds to revolutionize industries, from finance to healthcare, supply chain management to governance. By harnessing the power of blockchain, we aim to create a world where transactions are verifiable, data is secure, and individuals have greater control over their digital identities.
-                    </p>
+                
                     <p className="text-xs lg:text-md pt-2 tracking-wider">We are committed to pushing the boundaries of blockchain's capabilities, constantly innovating, and providing practical solutions that empower people and organizations. 
                     With a vision of a future where blockchain is seamlessly woven into the daily lives of individuals, MVoT is dedicated to leaving a lasting, positive impact on the world, ushering in an era of trust, efficiency, and inclusivity.
                     </p>
@@ -24,8 +22,8 @@ const About = () => {
                       <div className="flex items-center justify-center">
                         <img
                         src="https://images.pexels.com/photos/18318818/pexels-photo-18318818/free-photo-of-statue-of-liberty-at-night.png?auto=compress&cs=tinysrgb&w=600&lazy=load"
-                        className="items-center w-full lg:w-9/12 rounded-sm lg:ml-20 border border-gray-500 shadow-xl mx-auto"
-                        alt=""
+                        className="items-center w-full sm:w-full lg:w-10/12 rounded-md lg:ml-20 border border-gray-500 shadow-xl mx-auto"
+                        alt="Statue Of Liberty"
                         />
                       </div>                    
                     </div>
--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -24,7 +24,7 @@ const Services = () => {
                      
                   
                     </div>
-                    <div class="p-10 overflow-hidden shadow-lg items-center justify-center flex border-r-2 border-y-2 border-black">
+                    <div class="p-8 overflow-hidden shadow-lg items-center justify-center flex border-r-2 border-y-2 border-black">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 text-indigo-800">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
                             </svg>
@@ -32,7 +32,7 @@ const Services = () => {
                       
                 
                     </div>
-                    <div class="p-10 overflow-hidden shadow-lg items-center justify-center flex border-r-2 border-y-2 border-black">
+                    <div class="p-8 overflow-hidden shadow-lg items-center justify-center flex border-r-2 border-y-2 border-black">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 text-indigo-800">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 12.75c1.148 0 2.278.08 3.383.237 1.037.146 1.866.966 1.866 2.013 0 3.728-2.35 6.75-5.25 6.75S6.75 18.728 6.75 15c0-1.046.83-1.867 1.866-2.013A24.204 24.204 0 0112 12.75zm0 0c2.883 0 5.647.508 8.207 1.44a23.91 23.91 0 01-1.152 6.06M12 12.75c-2.883 0-5.647.508-8.208 1.44.125 2.104.52 4.136 1.153 6.06M12 12.75a2.25 2.25 0 002.248-2.354M12 12.75a2.25 2.25 0 01-2.248-2.354M12 8.25c.995 0 1.971-.08 2.922-.236.403-.066.74-.358.795-.762a3.778 3.778 0 00-.399-2.25M12 8.25c-.995 0-1.97-.08-2.922-.236-.402-.066-.74-.358-.795-.762a3.734 3.734 0 01.4-2.253M12 8.25a2.25 2.25 0 00-2.248 2.146M12 8.25a2.25 2.25 0 012.248 2.146M8.683 5a6.032 6.032 0 01-1.155-1.002c.07-.63.27-1.222.574-1.747m.581 2.749A3.75 3.75 0 0115.318 5m0 0c.427-.283.815-.62 1.155-.999a4.471 4.471 0 00-.575-1.752M4.921 6a24.048 24.048 0 00-.392 3.314c1.668.546 3.416.914 5.223 1.082M19.08 6c.205 1.08.337 2.187.392 3.314a23.882 23.882 0 01-5.223 1.082" />
                             </svg>
@@ -40,7 +40,7 @@ const Services = () => {
                    
                     
                     </div>
-                    <div class="p-10 overflow-hidden shadow-lg items-center justify-center flex border-x-1 border-y-2 border-black">
+                    <div class="p-8 overflow-hidden shadow-lg items-center justify-center flex border-x-1 border-y-2 border-black">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 text-indigo-800">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
                             </svg>
@@ -68,7 +68,7 @@ const Services = () => {
                             alt="MVoT Logo"
                         /> 
                         <div className='flex items-end justify-center 
-                        absolute inset-x-0 bottom-40 text-white text-5xl font-extralight'>01</div> 
+                        absolute inset-x-0 bottom-20 lg:bottom-40 text-white text-5xl font-extralight'>01</div> 
               
                     </div>
                     <div className="cols-span-1 relative group container bg-indigo-700 border-2 border-indigo-600 rounded-md">
@@ -78,7 +78,7 @@ const Services = () => {
                             alt="MVoT Logo"
                         /> 
                         <div className='flex items-end justify-center 
-                        absolute inset-x-0 bottom-40 text-white text-5xl font-extralight'>02</div> 
+                        absolute inset-x-0  bottom-20  lg:bottom-40 text-white text-5xl font-extralight'>02</div> 
               
                     </div>
                     <div className="cols-span-1 relative group container bg-indigo-700 border-2 border-indigo-600 rounded-md">
@@ -88,7 +88,7 @@ const Services = () => {
                             alt="MVoT Logo"
                         /> 
                         <div className='flex items-end justify-center 
-                        absolute inset-x-0 bottom-40 text-white text-5xl font-extralight'>03</div> 
+                        absolute inset-x-0  bottom-20  lg:bottom-40 text-white text-5xl font-extralight'>03</div> 
               
                     </div>
                     <div className="cols-span-1 relative group container bg-indigo-700 border-2 border-indigo-600 rounded-md">
@@ -98,7 +98,7 @@ const Services = () => {
                             alt="MVoT Logo"
                         /> 
                         <div className='flex items-end justify-center 
-                        absolute inset-x-0 bottom-40 text-white text-5xl font-extralight'>04</div> 
+                        absolute inset-x-0   bottom-20  lg:bottom-40 text-white text-5xl font-extralight'>04</div> 
               
                     </div>
 
